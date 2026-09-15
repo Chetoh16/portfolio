afed7715b2fcf337c586b968164fbfab70a01a17
Chetoh16 <egecetin1511@gmail.com>
2026-09-16 00:12:41 +0100

    feat: add filters setup

---
+++ filters.js
@@ -0,0 +1,7 @@
+const projectGrid = document.querySelector(".project-grid");
+const projectCards = projectGrid.querySelectorAll(".project-card");
+
+const filterState = {
+    type: "all",
+    language: "all"
+};
